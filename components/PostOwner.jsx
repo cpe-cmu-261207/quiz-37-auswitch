b2b7c597366368593d7d82ba266bd9328766a239
auswitch <sawit_cha@cmu.ac.th>
2022-09-15 14:48:14 +0700

    completed

--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import Comment from "./Comment";
+
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return <div className="vstack gap-3">
+  <div className="d-flex align-items-center gap-3">
+    <img
+      src="/profileImages/me.jpg"
+      width="48"
+      height="48"
+      className="rounded-circle"
+      style={{ objectFit: "cover" }}
+    />
+    <span className="fw-semibold fs-5">Switch Charuekpoonpol 640610672</span>
+  </div>
+  <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
+  <div className="d-flex align-items-center gap-1">
+    <img src="/like.svg" width={20}></img>
+    <span className="text-muted">100 คน</span>
+  </div>
+  <hr className="m-0" />
+  {
+        props.reply.map((c) => < Comment
+          name={c.username}
+          img={c.userImagePath}
+          text={c.commentText}
+          like={c.likeNum}
+          reply={c.replies}
+        />)
+      }
+</div>
 }
